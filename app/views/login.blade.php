4eedb5a0c176c4d90b99b0d0cbfd7d5755eca4f7
Ben Shewmaker <ben@shewbox.org>
2013-12-16 14:36:13 -0800

    Added login form

--- app/views/login.blade.php
+++ app/views/login.blade.php
@@ -6,4 +6,33 @@
 
 @section('content')
 <h3>Login page</h3>
+
+{{Form::open(array('url' => 'login', 'class' => 'form-horizontal'))}}
+		<div class="form-group">
+			<div class="col-sm-7">
+				<input class="form-control" name="username" type="text" placeholder="username">
+			</div>
+		</div>
+		<div class="form-group">
+			<div class="col-sm-7">
+				<input class="form-control" name="password" type="password" placeholder="password">
+			</div>
+		</div>
+		<div class="form-group">
+			<div class="col-sm-2">
+				<div class="checkbox">
+					<label>
+						<input name="remember" type="checkbox"> Remember Me
+					</label>
+				</div>
+			</div>
+		</div>
+		<div class="form-group">
+			<div class="col-sm-2">
+				<button type="submit" class="btn btn-primary">Submit</button>
+			</div>
+		</div>
+{{Form::close()}}
+
+
 @stop
