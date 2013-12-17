656677b78f85d42340a289a3aab4eb1906708ef8
Ben Shewmaker <ben@shewbox.org>
2013-12-17 10:09:35 -0800

    Added DB migrations for users and messages

---
+++ app/database/migrations/2013_12_17_174324_create_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('users', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->timestamps();
+			$table->string('username');
+			$table->string('password');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('users');
+	}
+
+}
---
+++ app/database/migrations/2013_12_17_174933_create_messages_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMessagesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('messages', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->timestamps();
+			$table->integer('user_id')->unsigned();
+			$table->string('message');
+		});
+
+		// Create foreign key for the user this message belongs to
+		Schema::table('messages', function(Blueprint $table)
+		{
+			$table->foreign('user_id')->references('id')->on('users')
+				->onDelete('cascade');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('messages');
+	}
+
+}
