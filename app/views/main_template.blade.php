6422bfa9655e741155ca1d8e86285a5c82f98e1d
Ben Shewmaker <ben@shewbox.org>
2013-12-19 14:12:45 -0800

    changed default theme to yeti because I like it.

--- app/views/main_template.blade.php
+++ app/views/main_template.blade.php
@@ -4,7 +4,7 @@
 	@yield('title')
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta charset="UTF-8">
-	{{HTML::style('bootstrap/css/bootstrap.min.css')}}
+	{{HTML::style('bootstrap/css/yeti.min.css')}}
 	@yield('stylesheets')
 </head>
 
