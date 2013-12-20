137b343ab8619886d29cc778cee6b31d750a339c
Ben Shewmaker <ben@shewbox.org>
2013-12-20 10:37:09 -0800

    Login form doesn’t show if user is logged in

--- app/views/login.blade.php
+++ app/views/login.blade.php
@@ -13,33 +13,37 @@
 	</div>
 @endif
 
-{{Form::open(array('url' => 'login', 'class' => 'form-horizontal'))}}
-		<div class="form-group">
-			<div class="col-sm-7">
-				<input class="form-control" name="username" type="text" placeholder="username">
-			</div>
-		</div>
-		<div class="form-group">
-			<div class="col-sm-7">
-				<input class="form-control" name="password" type="password" placeholder="password">
-			</div>
-		</div>
-		<div class="form-group">
-			<div class="col-sm-2">
-				<div class="checkbox">
-					<label>
-						<input name="remember" type="checkbox"> Remember Me
-					</label>
+@if(!Auth::check())
+	{{Form::open(array('url' => 'login', 'class' => 'form-horizontal'))}}
+			<div class="form-group">
+				<div class="col-sm-7">
+					<input class="form-control" name="username" type="text" placeholder="username">
 				</div>
 			</div>
-		</div>
-		<div class="form-group">
-			<div class="col-sm-2">
-				<button type="submit" class="btn btn-primary">Submit</button>
+			<div class="form-group">
+				<div class="col-sm-7">
+					<input class="form-control" name="password" type="password" placeholder="password">
+				</div>
 			</div>
-		</div>
-{{Form::close()}}
+			<div class="form-group">
+				<div class="col-sm-2">
+					<div class="checkbox">
+						<label>
+							<input name="remember" type="checkbox"> Remember Me
+						</label>
+					</div>
+				</div>
+			</div>
+			<div class="form-group">
+				<div class="col-sm-2">
+					<button type="submit" class="btn btn-primary">Submit</button>
+				</div>
+			</div>
+	{{Form::close()}}
+	{{HTML::link('register', 'Register')}}
+@else
+	<p>You are authenticated</p>
+@endif
 
-{{HTML::link('register', 'Register')}}
 
 @stop
