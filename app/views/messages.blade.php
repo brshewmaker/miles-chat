7b5842f22837a9b5c2f070a63d956d1c011613d9
Ben Shewmaker <ben@shewbox.org>
2013-12-24 16:18:07 -0800

    Forgot to add back the stripslashes

--- app/views/messages.blade.php
+++ app/views/messages.blade.php
@@ -1,4 +1,4 @@
 @foreach($messages as $message)
 	<?php $user = User::find($message->user_id); ?>
-	<tr><td data-messageid="{{$message->id}}"><p><i>{{substr($message->created_at, 11)}}</i> | <strong>{{$user->username}}</strong> {{$message->message}}</p></td></tr>
+	<tr><td data-messageid="{{$message->id}}"><p><i>{{substr($message->created_at, 11)}}</i> | <strong>{{$user->username}}</strong> {{stripslashes($message->message)}}</p></td></tr>
 @endforeach
