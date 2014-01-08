52e521021dece4aa89032fc45980b992e1b1b489
Ben Shewmaker <ben@shewbox.org>
2014-01-08 15:38:51 -0800

    Added uploads config file

---
+++ app/config/uploads.php
@@ -0,0 +1,15 @@
+<?php 
+
+/*
+|--------------------------------------------------------------------------
+| Path to Uploads
+|--------------------------------------------------------------------------
+| 
+| Give the full path to where miles-chat should store uploaded files.  
+| This path should be read/writable by the web server
+| 
+*/
+
+return array(
+	'path' => '',
+);
