1498a1af8b5250812e325a080236311bed0ba8d4
Ben Shewmaker <ben@shewbox.org>
2014-01-08 10:45:16 -0800

    Create the uploads DB migration.

---
+++ app/database/migrations/2014_01_08_183702_create_uploads_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUploadsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('uploads', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->timestamps();
+			$table->integer('user_id')->unsigned();
+			$table->string('filename');
+			$table->string('filetype');
+			$table->string('filesize');
+		});
+
+		// Create the foreign key for the user
+		Schema::table('uploads', function(Blueprint $table)
+		{
+			$table->foreign('user_id')->references('id')->on('users');
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('uploads');
+	}
+
+}
