a87d091cf2b17c7d4f0d3ccd607d9938790fce6e
Ben Shewmaker <ben@shewbox.org>
2014-01-10 23:25:42 +0000

    adding back in the uploads file

---
+++ app/config/uploads.php
@@ -0,0 +1,16 @@
+<?php 
+
+/*
+|--------------------------------------------------------------------------
+| Path to Uploads
+|--------------------------------------------------------------------------
+| 
+| Give the full path to where miles-chat should store uploaded files.  
+| This path should be read/writable by the web server and MUST end in 
+| a '/'
+| 
+*/
+
+return array(
+	'path' => '',
+);
