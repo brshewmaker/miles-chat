18d6941b7ad0bbd35be0502b93357bf3d0224a4a
Ben Shewmaker <ben@shewbox.org>
2014-01-16 16:28:39 -0800

    Change username text color

--- app/views/messages.blade.php
+++ app/views/messages.blade.php
@@ -2,7 +2,7 @@
 	<?php $user = User::find($message->user_id); ?>
 	<div class="chat-message">
 		<div class="chat-message-info">
-			<strong>{{$user->username}}</strong> | {{$message->created_at}}
+			<span class='text-muted'>{{$user->username}}</span> | {{$message->created_at}}
 		</div>
 		<div class="chat-message-body" data-messageid="{{$message->id}}">
 			<p>{{stripslashes($message->message)}}</p>
