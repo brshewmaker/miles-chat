3fec3290122b12e14ff2c6e2efd6aa26f28f96e2
Ben Shewmaker <ben@shewbox.org>
2014-02-13 21:50:36 -0800

    Changed the chat-messages to use a panel, for better separation and maybe a better look?

--- app/views/messages.blade.php
+++ app/views/messages.blade.php
@@ -1,10 +1,10 @@
 @foreach($messages as $message)
 	<?php $user = User::find($message->user_id); ?>
-	<div class="chat-message">
-		<div class="chat-message-info">
+	<div class="chat-message panel panel-default">
+		<div class="chat-message-info panel-heading">
 			<span class='text-muted'>{{$user->username}}</span> | {{$message->created_at}}
 		</div>
-		<div class="chat-message-body" data-messageid="{{$message->id}}">
+		<div class="chat-message-body panel-body" data-messageid="{{$message->id}}">
 			<p>{{$message->message}}</p>
 		</div>
 	</div>
