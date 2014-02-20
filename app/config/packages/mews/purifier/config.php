fbf915606b15368aada8e5dc3f2f899f2b48e3aa
Ben Shewmaker <ben@shewbox.org>
2014-02-19 20:42:26 -0800

    Apparently mews/htmlpurifier got updated after I got started, and the new update meant that my old code broke.  So this is a refactoring with the new way of doing the config files.

--- app/config/packages/mews/purifier/config.php
+++ app/config/packages/mews/purifier/config.php
@@ -1,11 +1,18 @@
-<?php 
+<?php
+
+/*
+ * This file is part of HTMLPurifier Bundle.
+ * (c) 2012 Maxime Dizerens
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 return array(
     "settings" => array(
         "default" => array(
             "HTML.SafeIframe" => 'true',
             "URI.SafeIframeRegexp" => "%^(http://|https://|//)(www.youtube.com/embed/|player.vimeo.com/video/)%",
-            "HTML.TargetBlank" => 'true',
         ),
         "titles" => array(
             'AutoFormat.AutoParagraph' => false,
