1f7ba00a85531cc1514fea916e7ddebd2902f225
Ben Shewmaker <ben@shewbox.org>
2014-06-13 14:00:24 -0700

    Added jsx compiler to grunt, and added that to the watch task so it should now concat all js files and then compile it to jsx

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,7 +4,7 @@ module.exports = function(grunt) {
 		watch: {
 			react: {
 				files: ['react_components/*.js'],
-				tasks: ['concat'],
+				tasks: ['concat', 'react'],
 			},
 		},
 
@@ -14,9 +14,19 @@ module.exports = function(grunt) {
 				dest: 'public/js/react-components.js',
 			},
 		},
-	});
+
+		react: {
+			single_file_output: {
+				files: {
+					'public/js/react-components.js': 'public/js/react-components.js'
+				}
+			},
+		},
+
+	}); // init Config
 
 	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-react');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 
 	grunt.registerTask('default', ['watch']);
