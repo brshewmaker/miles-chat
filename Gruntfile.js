634fa83df20aff652d6e7b0675f3b248e670d8bf
Ben Shewmaker <ben@shewbox.org>
2014-06-13 13:48:19 -0700

    Added command to concat all my react componenets

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,14 @@
 module.exports = function(grunt) {
 
+	grunt.initConfig({
+		concat: {
+			dist: {
+				src: ['react_components/*.js'],
+				dest: 'public/js/react-components.js',
+			},
+		},
+	});
+
 	grunt.loadNpmTasks('grunt-contrib-concat');
+	// grunt.loadNpmTasks('grunt-contrib-watch');
 };
