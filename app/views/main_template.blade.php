105b4f02ab360999912582d181f2ed64b00d36ea
Ben Shewmaker <ben@shewbox.org>
2014-06-13 14:06:47 -0700

    Replaced previous chat js with the new grunt/listened js

--- app/views/main_template.blade.php
+++ app/views/main_template.blade.php
@@ -64,6 +64,7 @@
 	{{HTML::script('bootstrap/js/bootstrap.min.js')}}
 	{{HTML::script('js/main.js')}}
 	{{HTML::script('js/react.js')}}
+	{{HTML::script('js/react-components.js')}}
 	@yield('scripts')
 </body>
 </html>
