dc39eb5630f1ebe902101add804d6cf4c54ef3ea
Ben Shewmaker <ben@shewbox.org>
2014-06-19 14:41:45 -0700

    Moved location on main.js

--- app/views/main_template.blade.php
+++ app/views/main_template.blade.php
@@ -62,8 +62,8 @@
 	<script type="text/javascript">var BASE = "<?php echo URL::to('/'); ?>";</script>
 	{{HTML::script('js/jquery-2.0.3.min.js')}}
 	{{HTML::script('bootstrap/js/bootstrap.min.js')}}
-	{{HTML::script('js/main.js')}}
 	{{HTML::script('js/react.js')}}
+	{{HTML::script('js/main.js')}}
 	{{HTML::script('js/react-components.js')}}
 	@yield('scripts')
 </body>
