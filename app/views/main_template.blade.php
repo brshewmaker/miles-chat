b448cbc882bc4e6d1e8224aa9d74c5209d196e41
Ben Shewmaker <ben@shewbox.org>
2014-06-25 11:25:00 -0700

    Pointing to the new location of 3rd party js libraries

--- app/views/main_template.blade.php
+++ app/views/main_template.blade.php
@@ -61,10 +61,10 @@
 
 
 	<script type="text/javascript">var BASE = "<?php echo URL::to('/'); ?>";</script>
-	{{HTML::script('js/jquery-2.0.3.min.js')}}
+	{{HTML::script('js/lib/jquery-2.0.3.min.js')}}
 	{{HTML::script('bootstrap/js/bootstrap.min.js')}}
-	{{HTML::script('js/react.js')}}
-	{{HTML::script('js/jquery.blockUI.js')}}
+	{{HTML::script('js/lib/react.js')}}
+	{{HTML::script('js/lib/jquery.blockUI.js')}}
 	{{HTML::script('js/main.js')}}
 	{{HTML::script('js/react-components.js')}}
 	@yield('scripts')
