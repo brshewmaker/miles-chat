9e3b8242c689b8fb5d76a849f29539e9bd9cf875
Ben Shewmaker <ben@shewbox.org>
2014-06-29 18:14:59 -0700

    Removing a comment from the previous commit

--- app/database/migrations/2014_06_27_212609_fulltext_index.php
+++ app/database/migrations/2014_06_27_212609_fulltext_index.php
@@ -8,9 +8,6 @@ class FulltextIndex extends Migration {
 	/**
 	 * Add a FULLTEXT search index on the table.  
 	 *
-	 * This requires changing the engine from innodb to myisam because innodb fulltext search
-	 * isn't available to versions of mysql < 5.6
-	 *
 	 * @return void
 	 */
 	public function up()
